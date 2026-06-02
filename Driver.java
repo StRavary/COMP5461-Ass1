636786de6fe9c61b87eea5c17103a5dded100ac7
Steven Ravary <54853545+StRavary@users.noreply.github.com>
2026-06-02 17:02:01 -0400

    first sync

---
+++ Driver.java
@@ -0,0 +1,55 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	
+    	 /*******************************************************************************************************************************************
+    	 */
+        Network objNetwork = new Network("network");
+        Server objServer = new Server();
+        Client clientSending = new Client("sending");
+        Client clientReceiving = new Client("receiving");
+        
+        // Create all 4 threads
+        Thread networkThread = new Thread(() -> objNetwork.run());
+        Thread serverThread = new Thread(() -> objServer.run());
+        Thread sendingThread = new Thread(() -> clientSending.run());
+        Thread receivingThread = new Thread(() -> clientReceiving.run());
+        
+        // Start all 4
+        networkThread.start();
+        serverThread.start();
+        sendingThread.start();
+        receivingThread.start();
+        
+        // Wait for all to complete
+        try {
+            networkThread.join();
+            serverThread.join();
+            sendingThread.join();
+            receivingThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        // Disconnect and terminate
+        objNetwork.disconnect(objNetwork.getClientIP());
+        objNetwork.disconnect(objNetwork.getServerIP());
+        System.out.println("\n Terminating network thread - Client " + objNetwork.getClientConnectionStatus() + " Server " + objNetwork.getServerConnectionStatus());
+    	
+    }
+}
