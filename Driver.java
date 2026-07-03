2c1eaae3f9808269f8bb49430548044545e9b1b4
remiperreault <62017616+remiperreault@users.noreply.github.com>
2026-07-03 07:44:03 -0400

    fixed server creation in main to match new constructor with no arguments

--- Driver.java
+++ Driver.java
@@ -19,15 +19,15 @@ public class Driver {
     	
     	 /*******************************************************************************************************************************************
     	 */
-        //Network objNetwork = new Network("network");
+        Network objNetwork = new Network();
         Server objServer1 = new Server(1);
         Server objServer2 = new Server(2);
         
         Client clientSending = new Client("sending");
         Client clientReceiving = new Client("receiving");
         
-        // Create all 4 threads
-        Thread networkThread = new Thread(() -> Network.run());
+        // Create all 5 threads
+        Thread networkThread = new Thread(() -> objNetwork.run());
         Thread serverThread1 = new Thread(() -> objServer1.run());
         Thread serverThread2 = new Thread(() -> objServer2.run());
 
